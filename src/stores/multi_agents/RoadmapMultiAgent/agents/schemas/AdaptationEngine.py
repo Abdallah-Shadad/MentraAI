9f0812614f9f112b6a8774b4ad14d48a58cc2d07
Abdul-Halim01 <eng.abdulhalem@gmail.com>
2026-04-18 05:29:55 +0200

    Enhance the AdaptionEngine Parser

--- src/stores/multi_agents/RoadmapMultiAgent/agents/schemas/AdaptationEngine.py
+++ src/stores/multi_agents/RoadmapMultiAgent/agents/schemas/AdaptationEngine.py
@@ -1,8 +1,13 @@
 from typing import List, Annotated
 from pydantic import BaseModel, Field
 
+class Resource(BaseModel):
+    name: Annotated[str, Field(description="Name of the resource")]
+    url: Annotated[str, Field(description="URL of the resource")]
+    type: Annotated[str, Field(description="Type of the resource")]
+
 class AdaptationEngineOutput(BaseModel):
-    topic: Annotated[str, Field(description="Topic of the resource")]
-    resources: Annotated[List[str], Field(description="List of resources for the topic")]
-    Where_in_stage_will_add_resource: Annotated[str, Field(description="Where in the stage will add the resource")] 
-    
+    stage_id: Annotated[str, Field(description="ID of the stage where will add to Roadmap")]
+    struggling_topics: Annotated[str, Field(description="Struggling topic what user get lower Accuarcy when solve Quiz")]
+    recommended_resources: Annotated[Resource, Field(description="Recommended resources Based on User point Failure")]
+    summary: Annotated[str, Field(description="Summary of the stage that will Encapsulate the Struggling topics and Recommended resources For user and Encourage him to work more")]
