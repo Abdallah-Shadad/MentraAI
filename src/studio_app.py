ae4004489244a3f3bd80eee50191ab865fee8a83
Abdul-Halim01 <eng.abdulhalem@gmail.com>
2026-04-18 05:28:11 +0200

    Use Gemini For higer Speed

--- src/studio_app.py
+++ src/studio_app.py
@@ -1,22 +1,29 @@
 from stores.multi_agents.RoadmapMultiAgent.RoadmapGraph import RoadmapGraph
 from stores.llm.providers.OpenAIProvider import OpenAIProvider
+from stores.llm.providers.GeminiProvider import GeminiProvider
 from stores.multi_agents.AgentProviderFactory import AgentProviderFactory
 
 config = {
-    "api_key": "sk-wfonewfofoofe",
-    "base_url": "https://8ae4-34-187-223-8.ngrok-free.app/v1/",
+    "api_key": "AIzaSyD7uC5nJHI5lXE1FCGX5jLOgxmfa8UmRVk",
+    "base_url": "https://59c4-34-148-170-199.ngrok-free.app/v1/",
     "max_output_tokens": 100000,
     "temperature": 0.1,
-    "model": "qwen3:8b",
+    "model": "qwen3.5:9b",
 }
 
-my_llm = OpenAIProvider(
+# my_llm = OpenAIProvider(
+#     api_key=config["api_key"],  
+#     base_url=config["base_url"],
+#     max_output_tokens=config["max_output_tokens"],
+#     temperature=config["temperature"],
+# )
+
+my_llm = GeminiProvider(
     api_key=config["api_key"],  
-    base_url=config["base_url"],
     max_output_tokens=config["max_output_tokens"],
     temperature=config["temperature"],
 )
-my_llm.set_generation_model(config["model"])
+my_llm.set_generation_model("gemini-2.5-flash-lite")
 
 
 agent_factory = AgentProviderFactory(config)
