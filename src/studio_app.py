7f06c41026dcfbc048942c466bff1991dd13d321
Abdo <aelwahsh29@gmail.com>
2026-05-06 11:09:59 +0300

    Quiz agent

--- src/studio_app.py
+++ src/studio_app.py
@@ -1,10 +1,14 @@
 from stores.multi_agents.RoadmapMultiAgent.RoadmapGraph import RoadmapGraph
+from stores.multi_agents.QuizAgent.QuizGraph import QuizGraph
 from stores.llm.providers.OpenAIProvider import OpenAIProvider
 from stores.llm.providers.GeminiProvider import GeminiProvider
 from stores.multi_agents.AgentProviderFactory import AgentProviderFactory
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 config = {
-    "api_key": "null",
+    "api_key": os.getenv('GEMINI_API_KEY'),
     "base_url": "https://8ae4-34-187-223-8.ngrok-free.app/v1/",
     "max_output_tokens": 100000,
     "temperature": 0.1,
@@ -28,7 +32,7 @@ my_llm.set_generation_model("gemini-2.5-flash-lite")
 
 agent_factory = AgentProviderFactory(config)
 
-graph = RoadmapGraph(
+graph = QuizGraph(
     config=config,
     agent_factory=agent_factory,
     llm=my_llm
