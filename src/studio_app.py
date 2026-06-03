349e670b056271e3b0bc4870db9bd376a266ae20
Abdo <aelwahsh29@gmail.com>
2026-06-03 18:02:06 +0300

    fixing Quiz generation Api Wrapping issue

--- src/studio_app.py
+++ src/studio_app.py
@@ -1,5 +1,5 @@
 #from stores.multi_agents.RoadmapMultiAgent.RoadmapGraph import RoadmapGraph
-#from stores.multi_agents.QuizAgent.QuizGraph import QuizGraph
+from stores.multi_agents.QuizAgent.QuizGraph import QuizGraph
 from stores.multi_agents.TrackRecommenderAgent.TrackRecommenderGraph import TrackRecommenderGraph
 from stores.multi_agents.ProjectAgent.ProjectGraph import ProjectGraph
 from stores.llm.providers.OpenAIProvider import OpenAIProvider
@@ -22,7 +22,7 @@ my_llm.set_generation_model("gemini-2.5-flash-lite")
 
 agent_factory = AgentProviderFactory(config)
 
-graph = TrackRecommenderGraph(
+graph = QuizGraph(
     config=config,
     agent_factory=agent_factory,
     llm=my_llm
