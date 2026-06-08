6893a15860a1cf1e7d65e48c63087f526d7d713a
ShehabAhmed1 <133771774+ShehabAhmed1@users.noreply.github.com>
2026-06-08 23:33:28 +0300

    lol

--- src/app/[locale]/student/contentPage/[id]/page.js
+++ src/app/[locale]/student/contentPage/[id]/page.js
@@ -20,9 +20,7 @@ export default function LessonPage() {
   const { data: resources1 } = useGetStageResources1(id);
   const { data: resources2 } = useGetStageResources2(id);
   const resources =
-    resources1?.data?.resources?.videos?.length > 0
-      ? resources1?.data?.resources
-      : resources2?.data?.resources || {};
+    resources1?.data?.resources || resources2?.data?.resources || {};
 
   console.log(resources);
 
