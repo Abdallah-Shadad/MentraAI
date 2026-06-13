c081b80e2795d07153f309848dc47f7eb6fdcfc2
Abdul-Halim01 <eng.abdulhalem@gmail.com>
2026-06-14 00:14:46 +0300

    Update the Langgraph studio

--- AI/src/studio_app.py
+++ AI/src/studio_app.py
@@ -1,7 +1,8 @@
-#from stores.multi_agents.RoadmapMultiAgent.RoadmapGraph import RoadmapGraph
+from stores.multi_agents.RoadmapMultiAgent.RoadmapGraph import RoadmapGraph
 from stores.multi_agents.QuizAgent.QuizGraph import QuizGraph
 from stores.multi_agents.TrackRecommenderAgent.TrackRecommenderGraph import TrackRecommenderGraph
 from stores.multi_agents.ProjectAgent.ProjectGraph import ProjectGraph
+# from stores.multi_agents.RoadmapAgent.RoadmapGraph import RoadmapGraph
 from stores.llm.providers.OpenAIProvider import OpenAIProvider
 from stores.llm.providers.GeminiProvider import GeminiProvider
 from stores.multi_agents.AgentProviderFactory import AgentProviderFactory
@@ -23,11 +24,27 @@ my_llm.set_generation_model("gemini-2.5-flash-lite")
 
 agent_factory = AgentProviderFactory(config)
 
-graph = QuizGraph(
-    config=config,
-    agent_factory=agent_factory,
-    llm=my_llm
-)
+# Quiz Graph
+# graph = QuizGraph(
+#     config=config,
+#     agent_factory=agent_factory,
+#     llm=my_llm
+# )
+# Roadmap Graph
+# graph = RoadmapGraph(
+#     config=config,
+#     agent_factory=agent_factory,
+#     llm=my_llm
+# )
+
+# Track Recomender 
+# graph = TrackRecommenderGraph(
+#     config=config,
+#     agent_factory=agent_factory,
+#     llm=my_llm
+# )
+
+
 
 # In LangGraph Studio must Called Graph = app
 app = graph.build().compile()
